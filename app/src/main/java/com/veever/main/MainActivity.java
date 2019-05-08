2110583f5b13e83db0f5557b683334cfb2e7788d
AndrewsAlves <andrewsalvisbusiness@gmail.com>
2019-05-08 15:15:38 +0530

    beacon detected

--- app/src/main/java/com/veever/main/MainActivity.java
+++ app/src/main/java/com/veever/main/MainActivity.java
@@ -1,29 +1,45 @@
 package com.veever.main;
 
+import android.Manifest;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
 import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
 import org.altbeacon.beacon.startup.BootstrapNotifier;
 import org.altbeacon.beacon.startup.RegionBootstrap;
 
+import java.util.Collection;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity implements BootstrapNotifier {
+public class MainActivity extends AppCompatActivity implements BootstrapNotifier , BeaconConsumer {
 
     private static final String TAG = "MainActivity";
 
@@ -43,8 +59,7 @@ public class MainActivity extends AppCompatActivity implements BootstrapNotifier
 
     private RegionBootstrap regionBootstrap;
     private BackgroundPowerSaver backgroundPowerSaver;
-    private boolean haveDetectedBeaconsSinceBoot = false;
-
+    private BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,59 +67,15 @@ public class MainActivity extends AppCompatActivity implements BootstrapNotifier
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        BeaconManager beaconManager = org.altbeacon.beacon.BeaconManager.getInstanceForApplication(this);
-        BeaconManager.setDebug(true);
 
-        // Uncomment the code below to use a foreground service to scan for beacons. This unlocks
-        // the ability to continually scan for long periods of time in the background on Andorid 8+
-        // in exchange for showing an icon at the top of the screen and a always-on notification to
-        // communicate to users that your app is using resources in the background.
-        //
-
-
-        Notification.Builder builder = new Notification.Builder(this);
-        builder.setSmallIcon(R.drawable.ic_launcher);
-        builder.setContentTitle("Scanning for Beacons");
-        Intent intent = new Intent(this, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(
-                this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT
-        );
-        builder.setContentIntent(pendingIntent);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel("My Notification Channel ID",
-                    "My Notification Name", NotificationManager.IMPORTANCE_DEFAULT);
-            channel.setDescription("My Notification Channel Description");
-            NotificationManager notificationManager = (NotificationManager) getSystemService(
-                    Context.NOTIFICATION_SERVICE);
-            notificationManager.createNotificationChannel(channel);
-            builder.setChannelId(channel.getId());
-        }
-        beaconManager.enableForegroundServiceScanning(builder.build(), 456);
-        // For the above foreground scanning service to be useful, you need to disable
-        // JobScheduler-based scans (used on Android 8+) and set a fast background scan
-        // cycle that would otherwise be disallowed by the operating system.
-        //
-        beaconManager.setEnableScheduledScanJobs(false);
-        beaconManager.setBackgroundBetweenScanPeriod(0);
-        beaconManager.setBackgroundScanPeriod(1100);
-
-
-        /*Log.d(TAG, "setting up background monitoring for beacons and power saving");
+        // setting beacon regions
+        Log.d(TAG, "setting up background monitoring for beacons and power saving");
         // wake up the app when a beacon is seen
         Region region = new Region("backgroundRegion",
                 null, null, null);
         regionBootstrap = new RegionBootstrap(this, region);
-
-        // simply constructing this class and holding a reference to it in your custom Application
-        // class will automatically cause the BeaconLibrary to save battery whenever the application
-        // is not visible.  This reduces bluetooth power usage by about 60%
         backgroundPowerSaver = new BackgroundPowerSaver(this);
-
-        // If you wish to test beacon detection in the Android Emulator, you can use code like this:
-        // BeaconManager.setBeaconSimulator(new TimedBeaconSimulator() );
-        // ((TimedBeaconSimulator) BeaconManager.getBeaconSimulator()).createTimedSimulatedBeacons();*/
-
-
+        beaconManager.bind(this);
     }
 
     @Override
@@ -117,65 +88,6 @@ public class MainActivity extends AppCompatActivity implements BootstrapNotifier
     protected void onStop() {
         super.onStop();
     }
-
-    public void disableMonitoring() {
-        if (regionBootstrap != null) {
-            regionBootstrap.disable();
-            regionBootstrap = null;
-        }
-    }
-
-    public void enableMonitoring() {
-        Region region = new Region("backgroundRegion",
-                null, null, null);
-        regionBootstrap = new RegionBootstrap(this, region);
-    }
-
-    @Override
-    public void didEnterRegion(Region arg0) {
-        // In this example, this class sends a notification to the user whenever a Beacon
-        // matching a Region (defined above) are first seen.
-        Log.d(TAG, "did enter region.");
-        if (!haveDetectedBeaconsSinceBoot) {
-            Log.d(TAG, "auto launching MainActivity");
-
-            // The very first time since boot that we detect an beacon, we launch the
-            // MainActivity
-           // Intent intent = new Intent(this, MonitoringActivity.class);
-         //   intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            Log.d(TAG, "didEnterRegion() called with: arg0 = [" + arg0 + "]");
-            // Important:  make sure to add android:launchMode="singleInstance" in the manifest
-            // to keep multiple copies of this activity from getting created if the user has
-            // already manually launched the app.
-            //this.startActivity(intent);
-            haveDetectedBeaconsSinceBoot = true;
-        } else {
-          /*  if (monitoringActivity != null) {
-                // If the Monitoring Activity is visible, we log info about the beacons we have
-                // seen on its display
-                logToDisplay("I see a beacon again" );
-            } else {
-                // If we have already seen beacons before, but the monitoring activity is not in
-                // the foreground, we send a notification to the user on subsequent detections.
-                Log.d(TAG, "Sending notification.");
-                sendNotification();
-            }*/
-        }
-
-
-    }
-
-    @Override
-    public void didExitRegion(Region region) {
-        Log.e(TAG, "didExitRegion: I no longer see a beacon.");
-    }
-
-    @Override
-    public void didDetermineStateForRegion(int state, Region region) {
-        Log.d(TAG, "didDetermineStateForRegion() called with: state = [" + state + "], region = [" + region + "]");
-    }
-
-
 
     ////////////////////
     ///// OnClick
@@ -198,6 +110,8 @@ public class MainActivity extends AppCompatActivity implements BootstrapNotifier
         textViewVeeverStatus.setTextColor(getResources().getColor(R.color.lime2));
         imageButtonActivate.setImageResource(R.drawable.button_eye_on);
 
+        startBeaconMonitoring();
+
         isActivated = true;
     }
 
@@ -208,7 +122,91 @@ public class MainActivity extends AppCompatActivity implements BootstrapNotifier
         textViewVeeverStatus.setTextColor(getResources().getColor(R.color.veeverwhite));
         imageButtonActivate.setImageResource(R.drawable.button_eye_off);
 
+        disableMonitoring();
 
         isActivated = false;
+    }
+
+    public void startBeaconMonitoring() {
+
+        Dexter.withActivity(this)
+                .withPermissions(Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.BLUETOOTH_ADMIN)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        Log.d(TAG, "onPermissionsChecked() called with: report = [" + report + "]");
+                        enableBluetooth();
+                        enableMonitoring();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+
+                    }
+                })
+                .check();
+    }
+
+    public void enableBluetooth() {
+        final BluetoothAdapter bAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bAdapter == null) {
+            Log.e(TAG, "onPermissionsChecked: bluetooth service not available");
+        } else {
+            if (!bAdapter.isEnabled()) {
+                startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), 1);
+                Log.e(TAG, "onPermissionsChecked: bluetooth turned ON");
+            }
+        }
+    }
+
+    public void disableMonitoring() {
+        if (regionBootstrap != null) {
+            regionBootstrap.disable();
+            regionBootstrap = null;
+        }
+    }
+
+    public void enableMonitoring() {
+        Region region = new Region("backgroundRegion",
+                null, null, null);
+        regionBootstrap = new RegionBootstrap(this, region);
+    }
+
+    @Override
+    public void didEnterRegion(Region region) {
+
+    }
+
+    @Override
+    public void didExitRegion(Region region) {
+
+    }
+
+    @Override
+    public void didDetermineStateForRegion(int i, Region region) {
+
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        RangeNotifier rangeNotifier = new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                if (beacons.size() > 0) {
+                    Log.d(TAG, "didRangeBeaconsInRegion called with beacon count:  "+beacons.size());
+                    Beacon firstBeacon = beacons.iterator().next();
+                    Log.e(TAG, "didRangeBeaconsInRegion: " + firstBeacon.toString());
+                   // logToDisplay("The first beacon " + firstBeacon.toString() + " is about " + firstBeacon.getDistance() + " meters away.");
+                }
+            }
+
+        };
+        try {
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+            beaconManager.addRangeNotifier(rangeNotifier);
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+            beaconManager.addRangeNotifier(rangeNotifier);
+        } catch (RemoteException e) {   }
     }
 }
--- app/src/main/java/com/veever/main/VeeverApplication.java
+++ app/src/main/java/com/veever/main/VeeverApplication.java
@@ -5,6 +5,9 @@ import android.app.Application;
 import com.veever.main.manager.APIManager;
 import com.veever.main.manager.DatabaseManager;
 
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+
 public class VeeverApplication extends Application {
 
     @Override
@@ -14,6 +17,12 @@ public class VeeverApplication extends Application {
         DatabaseManager.initialize(this);
         APIManager.initialize(this);
 
+        BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
+
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25")); //Altbeacon layout
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24")); // ibeacon layout
+
+        beaconManager.setDebug(true);
     }
 
     // api key nearby = AIzaSyCrnrHAF6Z87vHhwVAV60muPjeoKaV7Yhw
