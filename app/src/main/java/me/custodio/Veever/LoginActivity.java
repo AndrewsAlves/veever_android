763e15ab76553cddf508d947347f8e5b1adde1d3
AndrewsAlves <andrewsalvisbusiness@gmail.com>
2019-09-19 00:13:05 +0530

    Facebook login working!

--- app/src/main/java/me/custodio/Veever/LoginActivity.java
+++ app/src/main/java/me/custodio/Veever/LoginActivity.java
@@ -4,13 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookRequestError;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.Arrays;
 import java.util.UUID;
 
 import butterknife.BindView;
@@ -23,8 +38,11 @@ import me.custodio.Veever.manager.FirestoreManager;
 import me.custodio.Veever.manager.SharedPrefsManager;
 import me.custodio.Veever.views.IndeterminantProgressBar;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements FacebookCallback<LoginResult> {
 
+    private static final String TAG = "LoginActivity";
+
+    private static final String EMAIL = "email";
 
     @BindView(R.id.progressBar_guest)
     IndeterminantProgressBar progressBarGuest;
@@ -38,18 +56,91 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.ll_faceboook_btn_content)
     LinearLayout parentFacebookContent;
 
+    @BindView(R.id.facebook_login_button)
+    LoginButton fbGhostBotton;
+
+    private CallbackManager callbackManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
         EventBus.getDefault().register(this);
+
+        callbackManager = CallbackManager.Factory.create();
+        fbGhostBotton.setReadPermissions(Arrays.asList(EMAIL));
+
+        LoginManager.getInstance().registerCallback(callbackManager, this);
+
     }
 
     @Override
     protected void onDestroy() {
         EventBus.getDefault().unregister(this);
         super.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onSuccess(LoginResult o) {
+        requestUserProfile(o);
+        Log.d(TAG, "onSuccess() called with: o = [" + o + "]");
+    }
+
+    @Override
+    public void onCancel() {
+        Log.d(TAG, "onCancel() called");
+    }
+
+    @Override
+    public void onError(FacebookException error) {
+        Log.d(TAG, "onError() called with: error = [" + error + "]");
+    }
+
+    public void requestUserProfile(LoginResult result) {
+
+        GraphRequest request = GraphRequest.newMeRequest(
+                result.getAccessToken(),
+                (object, response) -> {
+
+                    Log.e(TAG, "onCompleted: graphResponce: " + response.toString());
+
+                    try {
+
+                        progressBarFacebook.setVisibility(View.VISIBLE);
+                        parentFacebookContent.setVisibility(View.GONE);
+
+                        String email = object.getString("email");
+                        String id = object.getString("id");
+                        String firstName = object.getString("first_name");
+                        String lastName = object.getString("last_name");
+
+                        User user = new User(object.getString("id"));
+                        user.setEmail(email);
+                        user.setFirstName(firstName);
+                        user.setLastName(lastName);
+                        user.setUserId(id);
+
+                        SharedPrefsManager.saveUserId(this, id);
+                        FirestoreManager.getInstance().createNewUser(user);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,first_name,last_name,email");
+        request.setParameters(parameters);
+        request.executeAsync();
+        request.executeAsync();
+
     }
 
     @OnClick(R.id.ll_btn_login_guest)
@@ -73,16 +164,14 @@ public class LoginActivity extends AppCompatActivity {
 
     @OnClick(R.id.ll_btn_login_facebook)
     public void clickFacebook() {
-
-        progressBarFacebook.setVisibility(View.VISIBLE);
-        parentFacebookContent.setVisibility(View.GONE);
-
+        fbGhostBotton.performClick();
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onMessageEvent(UserSignUpSuccesEvent event) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+        finish();
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
