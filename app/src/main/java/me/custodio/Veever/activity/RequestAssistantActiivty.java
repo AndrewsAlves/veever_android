736c4e78ca483ce3b0179e15e02fa7a061cd0314
AndrewsAlves <andrewsalvisbusiness@gmail.com>
2019-09-19 23:35:23 +0530

    Request assistance screen and lottie

--- app/src/main/java/me/custodio/Veever/activity/RequestAssistantActiivty.java
+++ app/src/main/java/me/custodio/Veever/activity/RequestAssistantActiivty.java
@@ -2,15 +2,42 @@ package me.custodio.Veever.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.os.Bundle;
 
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieProperty;
+import com.airbnb.lottie.model.KeyPath;
+import com.airbnb.lottie.value.LottieFrameInfo;
+import com.airbnb.lottie.value.SimpleLottieValueCallback;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import me.custodio.Veever.R;
 
 public class RequestAssistantActiivty extends AppCompatActivity {
+
+    @BindView(R.id.lottie_request_assistance)
+    LottieAnimationView lottieAnimationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_assistant_actiivty);
+        ButterKnife.bind(this);
+
+        lottieAnimationView.addValueCallback(
+                new KeyPath("**"),
+                LottieProperty.COLOR_FILTER,
+                new SimpleLottieValueCallback<ColorFilter>() {
+                    @Override
+                    public ColorFilter getValue(LottieFrameInfo<ColorFilter> frameInfo) {
+                        return new PorterDuffColorFilter(getResources().getColor(R.color.veeverblack), PorterDuff.Mode.SRC_ATOP);
+                    }
+                }
+        );
     }
 }
--- app/src/main/java/me/custodio/Veever/fragment/RequestAssistantFragment.java
+++ app/src/main/java/me/custodio/Veever/fragment/RequestAssistantFragment.java
@@ -37,7 +37,6 @@ public class RequestAssistantFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
